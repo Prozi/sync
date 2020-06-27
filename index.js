eab2cb02e77fa4daa8d171724757532aec1eb111
Jacek Pietal <prozi85@gmail.com>
2020-06-27 21:50:59 +0200

    fix(tests): update tests

--- index.js
+++ index.js
@@ -2,13 +2,4 @@
 
 import { Flow } from "./lib/flow";
 
-const flow = new Flow(null, {
-  debug: true,
-  plugins: {
-    echo: function (ws, { id, event, data }) {
-      flow.api.broadcast(ws, { id, event, data });
-    },
-  },
-});
-
-export default flow;
+export default new Flow({ debug: true, port: 8080 });
