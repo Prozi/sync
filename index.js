c44eb7cfb90f94b701f82c8a1e7f90a696d463eb
Jacek Pietal <prozi85@gmail.com>
2020-06-27 21:30:19 +0200

    feat(demo): show multiplayer state in demo

--- index.js
+++ index.js
@@ -2,4 +2,13 @@
 
 import { Flow } from "./lib/flow";
 
-export default Flow;
+const flow = new Flow(null, {
+  debug: true,
+  plugins: {
+    echo: function (ws, { id, event, data }) {
+      flow.api.broadcast(ws, { id, event, data });
+    },
+  },
+});
+
+export default flow;
