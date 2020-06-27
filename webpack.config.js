4d69aac2bde00b39cf091bb82166077e2a5ddc06
Jacek Pietal <prozi85@gmail.com>
2020-06-27 18:28:03 +0200

    feat(joystick): unify joystick code

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./lib/demo.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+  output: {
+    path: __dirname + "/demo",
+    publicPath: "/",
+    filename: "dist.js",
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: "./demo",
+    hot: true,
+  },
+  externals: {
+    "@jacekpietal/bouncer.js": __dirname + '/node_modules/bouncer.js/index.js',
+  },
+};
